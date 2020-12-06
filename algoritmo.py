9f3db1fff86eee05f36eee6a7913654ae4d15635
PABLO CORCOLES MOLINA <Pablo.Corcoles2@alu.uclm.es>
2020-12-06 22:39:52 +0100

    Algoritmo terminado

--- algoritmo.py
+++ algoritmo.py
@@ -1,30 +1,22 @@
-#ALGORITMO DE LA WIKIPEDIA EN C# FUNCIÓN EXPONENCIACIÓN MODULAR
-#
-# Bignum modpow(Bignum base, Bignum exp, Bignum m) {
-#
-#    Bignum result = 1;
-#
-#    while (exp > 0) {
-#       if ((exp & 1) > 0) result = (result * base) % m;
-#       exp >>= 1;
-#       base = (base * base) % m;
-#    }
-#
-#    return result;
-#
-#  }
 
-# Función de exponenciación rápida modular
-# def exp_rapida(base, exponente, modulo):
-# 	x = 1
-# 	y = base % modulo
-# 	b = exponente
-# 	while (b > 0):
-# 		if ((b % 2) == 0):  # Si b es par...
-# 			y = (y * y) % modulo
-# 			b = b / 2
-# 		else:  # Si b es impar...
-# 			x = (x * y) % modulo
-# 			b = b - 1
-# 	return x
+# n es la base, x es el exponente y p es el modulo
+# z = n^x mod p
+def exponenciacion(n, x, p):
+    z = 1
+    y = n % p
+    while (x > 0):
+        if ((x % 2) == 0):  # Si b es par
+            y = (y * y) % p
+            x = x / 2
+        else:  # Si b es impar
+            z = (z * y) % p
+            x = x - 1
+    return z
+
+
+
+
+if __name__ == '__main__':
+    result = exponenciacion(50, 3, 461)
+    print(result)
 
